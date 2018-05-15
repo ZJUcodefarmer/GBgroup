c552e69ab0e5ecf7ccad8cde9ca8389e4d35cb6f
ZJUcodefarmer <39267048+ZJUcodefarmer@users.noreply.github.com>
2018-05-15 21:48:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,48 +13,52 @@ server.on('request',function(req,res)
 	if(url=='/')mark=0;
 	if(mark==0)
 	{
-	var fullpath="./html"+url;
-	console.log('The URL is : '+url);
-	if(url=='/')
-	{
-		sr=fullpath+'index';
-		fullpath=fullpath+'index/index.html';
-		mark=1;
-	}
-	else 
-	{
-		sr=fullpath;
-		fullpath=fullpath+url+'.html';
-		mark=1;
-	}
-	console.log(fullpath);
-	fs.readFile(fullpath,function(err,data)
-	{
-		if(err)
+		var fullpath="./html"+url;
+		console.log('The URL is : '+url);
+		if(url=='/')
 		{
-			fs.readFile('./html/404/404.html','utf8',function(ERR,DATA)
-			{
-				if(ERR)
-				{
-					throw ERR;
-				}
-				sr='./html/404';
-				mark=1;
-				res.end(DATA);
-			});
-			return;
+			sr=fullpath+'index';
+			fullpath=fullpath+'index/index.html';
+			mark=1;
 		}
+		else 
+		{
+			sr=fullpath;
+			fullpath=fullpath+url+'.html';
+			mark=1;
+		}
+		console.log(fullpath);
+		fs.readFile(fullpath,function(err,data)
+		{
+			if(err)
+			{
+			fs.readFile('./html/404/404.html','utf8',function(ERR,DATA)
+				{
+					if(ERR)
+					{
+						throw ERR;
+					}
+					sr='./html/404';
+					mark=1;
+					res.end(DATA);
+				});
+				return;
+			}
 		res.end(data);
-	});
+		});
 	}
 	else
 	{
+		if(url=='favicon.ico')
+		{
+			return;
+		}
 		var fullpath=sr+url;
 		console.log(fullpath);
-	fs.readFile(fullpath,function(err,data)
-	{
-		if(err)
+		fs.readFile(fullpath,function(err,data)
 		{
+			if(err)
+			{
 			fs.readFile('./html/404/404.html','utf8',function(ERR,DATA)
 			{
 				if(ERR)
@@ -66,9 +70,9 @@ server.on('request',function(req,res)
 				res.end(DATA);
 			});
 			return;
-		}
-		res.end(data);
-	});
+			}
+			res.end(data);
+		});
 	}
 });
 server.listen(3000,function()
